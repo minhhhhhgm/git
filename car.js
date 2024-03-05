52e6809a3e24c96527e5099facdad8e340c0defd
minhhhhhgm <162268777+minhhhhhgm@users.noreply.github.com>
2024-03-05 10:55:33 +0700

    Update test.js
    add new car
    
    Update car.js

---
+++ car.js
@@ -0,0 +1,3 @@
+const stop = ()=>{
+    console.log("STOPCAR");
+}
--- test.js
+++ test.js
@@ -1,9 +1,17 @@
 const hello =()=>{
+<<<<<<< HEAD
     // console.log("hello");
+=======
+    console.log("helloUpdate");
+>>>>>>> 96373eb (Update test.js)
 }
 
 
 const goodbye =()=>{
     console.log("goodbye2222");
+<<<<<<< HEAD
     console.log("goodbye 100");
 }
+=======
+}
+>>>>>>> 96373eb (Update test.js)
